131f31506174d8e5df528f03388bf9776b757aeb
Adam Eivy <adam.eivy@disney.com>
2013-02-26 19:26:46 -0800

    update demo to match repo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #License#
-@author Adam Eivy (antic | atomantic) 
+@author Adam Eivy (antic | atomantic)  
 @link [http://intellectualpirates.net](http://intellectualpirates.net)  
 
 @license Copyright (c) 2010 Adam Eivy (antic | atomantic) Dual licensed under the MIT and GPL licenses:  
@@ -13,24 +13,60 @@ This is a simple wrapper for the Google Maps API to load a map and handle a star
 #Usage:#
 
 1. Get a Google Maps API Key: [http://code.google.com/apis/maps/signup.html](http://code.google.com/apis/maps/signup.html)
-2. Include the needed js files:
+2. Include the needed HTML:
 
-  <!-- Include jQuery >= 1.4.2: [http://paulirish.com/2010/the-protocol-relative-url/](http://paulirish.com/2010/the-protocol-relative-url/) -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
-  <!-- non google fallback -->
-  <script>!window.jQuery && document.write(unescape('%3Cscript src="js/lib/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
-  <!-- Google Maps API library (Make sure to replace YOUR_API_KEY_HERE) -->
-  <script type="text/javascript" src="http://maps.google.com/maps?file=api&v=2&sensor=true&key=YOUR_API_KEY_HERE"></script>
-  <!-- Load the gmapDirections wrapper -->
-  <script type="text/javascript" src="/js/lib/jquery.gmapDirections.js?BUILD_VERSION_NUMBER"></script>
+        <label>Start:</label><input type="text" id="StartAddress" value="Seattle" />
+        <label>End:</label><input type="text" id="EndAddress" value="Portland" />
+        <input type="submit" id="GetDirections" value="Get Directions" />
+        <div id="Directions"></div>
+        <div id="Map"></div>
 
-  <!-- call the plugin -->
-	<script type="text/javascript">
-		$(funciton(){
-			$('#Map').gmapDirections();
-		})
-	</script>
-	
+3. Include the needed js files:
+
+        <!-- Include latest jQuery. Tested with jQuery >= 1.4.2: http://paulirish.com/2010/the-protocol-relative-url/-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>     
+    
+        <!-- non google fallback -->
+        <script>!window.jQuery && document.write(unescape('%3Cscript src="jquery.min.js"%3E%3C/script%3E'))</script>  
+        
+        <!-- Google Maps API library (Make sure to replace YOUR_API_KEY_HERE) -->
+        <script type="text/javascript" src="http://maps.google.com/maps?file=api&v=2&sensor=true&key=YOUR_API_KEY_HERE"></script>
+
+        <!-- Load the gmapDirections wrapper -->
+        <script type="text/javascript" src="jquery.gmapDirections.js?BUILD_VERSION_NUMBER"></script>
+
+        <!-- call the plugin -->
+        <script type="text/javascript">
+            $(function(){
+                $('#Map').gmapDirections();
+            })
+        </script>
+
+4. You can override the element IDs for all the needed parts in the config object passed to the plugin. Here it is called with all the defaults:
+
+        <script type="text/javascript">
+            $(function(){
+                $('#Map').gmapDirections({
+                    center: 'Seattle',             // a valid address or location search to start with
+                    directions: 'Directions',      // element ID to hold directions printout
+                    end: 'EndAddress',             // element ID for end address textfield
+                    iconHeight: 50,                // height of custom icon image
+                    iconImg: false,                // custom icon image?
+                    iconWidth: 50,                 // width of custom icon image
+                    onFail: function(){            // failed loading center element
+                        alert('could not find address');
+                    },    
+                    onFinish: function(){          // after direction load
+                    },
+                    start: 'StartAddress',         // element ID for start address textfield
+                    submit: 'GetDirections',       // element ID for the action button
+                    tooltip: false,                // custom tooltip?
+                    zoom: 15                       // zoom level - refer to google maps API specs
+                });
+            })
+        </script>
+
+
 #Demos:#
 
 The repository holds a demo in index.html, which is published on the github project page:
--- jquery.gmapDirections.js
+++ jquery.gmapDirections.js
@@ -11,13 +11,13 @@
  */
 (function($, window, document, undefined) {
     /**
-	 * The core plugin object
-	 * 
-	 * @param dom el The main element to attach directions
-	 * @param obj opt Configuration option overrides
-	 *
-	 * @return obj an instance of the constructed library object
-	 */
+     * The core plugin object
+     * 
+     * @param dom el The main element to attach directions
+     * @param obj opt Configuration option overrides
+     *
+     * @return obj an instance of the constructed library object
+     */
     var gmapDirections = window.gmapDirections = function(el, opt) {
         var o = $.extend({},
         gmapDirections.defaults, opt || '');
@@ -28,51 +28,51 @@
         this.map = null;
         this.el = el;
         this.opt = o;
-		this.elDirections = $('#'+o.directions);
-		this.elStart = $('#'+o.start);
-		this.elEnd = $('#'+o.end);
-		this.elSubmit = $('#'+o.submit);
+        this.elDirections = $('#'+o.directions);
+        this.elStart = $('#'+o.start);
+        this.elEnd = $('#'+o.end);
+        this.elSubmit = $('#'+o.submit);
         this.load();
         return this;
     };
     /**
-	 * loader
-	 */
+     * loader
+     */
     gmapDirections.prototype.load = function() {
-	
+    
         this.init();
-		var opt = this.opt,
-			map = this.map,
-			obj = this, // store context for submit
-			marker,
-			markerConf = {};
+        var opt = this.opt,
+            map = this.map,
+            obj = this, // store context for submit
+            marker,
+            markerConf = {};
 
         this.elSubmit.click(function(e) {
-			// in case of default form handling to submit (kill it)
+            // in case of default form handling to submit (kill it)
             e.preventDefault();
-			// clear previous directions (just in case)
+            // clear previous directions (just in case)
             obj.elDirections.html('');
             map.clearOverlays();
             (new GDirections(map, obj.elDirections.get(0))).load("from: " + obj.elStart.val() + " to: " + obj.elEnd.val());
-			opt.onFinish();
-		});
+            opt.onFinish();
+        });
         if (!this.geo) {
-			return this;
-		}
+            return this;
+        }
 
         this.geo.getLatLng(opt.center, function(point) {
             if (!point) {
-				opt.onFail();
-				return false;
+                opt.onFail();
+                return false;
             }
             map.setCenter(point, opt.zoom);
 
             if (opt.iconImg) {
                 customicon = new GIcon(G_DEFAULT_ICON, opt.iconImg);
                 customicon.iconSize = new GSize(opt.iconWidth, opt.iconHeight);
-				markerConf.icon = customicon;
+                markerConf.icon = customicon;
             }
-         	marker = new GMarker(point,markerConf);
+             marker = new GMarker(point,markerConf);
             map.addOverlay(marker);
 
             if (opt.tooltip) {
@@ -83,8 +83,8 @@
         return this;
     };
     /**
-	 * initializer
-	 */
+     * initializer
+     */
     gmapDirections.prototype.init = function() {
         if (!this.map) {
             if (GBrowserIsCompatible()) {
@@ -95,40 +95,40 @@
         }
     };
     /**
-	 * configuration defaults
-	 */
+     * configuration defaults
+     */
     gmapDirections.defaults = {
-     	center: 'Seattle', 			// a valid address or location search to start with
-        directions: 'Directions',	// element ID to hold directions printout
-        end: 'EndAddress',			// element ID for end address textfield
-        iconHeight: 50, 			// height of custom icon image
-        iconImg: false,				// custom icon image?
-        iconWidth: 50, 				// width of custom icon image
-		onFail: function(){			// failed loading center element
-			alert('could not find address');
-		},	
-		onFinish: function(){		// after direction load
-		},
-        start: 'StartAddress',		// element ID for start address textfield
-        submit: 'GetDirections',	// element ID for the action button
-        tooltip: false,				// custom tooltip?
-        zoom: 15					// zoom level - refer to google maps API specs
+         center: 'Seattle',         // a valid address or location search to start with
+        directions: 'Directions',   // element ID to hold directions printout
+        end: 'EndAddress',          // element ID for end address textfield
+        iconHeight: 50,             // height of custom icon image
+        iconImg: false,             // custom icon image?
+        iconWidth: 50,              // width of custom icon image
+        onFail: function(){         // failed loading center element
+            alert('could not find address');
+        },    
+        onFinish: function(){       // after direction load
+        },
+        start: 'StartAddress',      // element ID for start address textfield
+        submit: 'GetDirections',    // element ID for the action button
+        tooltip: false,             // custom tooltip?
+        zoom: 15                    // zoom level - refer to google maps API specs
     };
 
     /**
-	 * jQuery fn
-	 * 
-	 * @param obj opt The config options
-	 * 
-	 * @return instance of gmapDirections
-	 */
+     * jQuery fn
+     * 
+     * @param obj opt The config options
+     * 
+     * @return instance of gmapDirections
+     */
     $.fn.gmapDirections = function(opt) {
         if (!this.length) {
             return this; // nothing to do
         }
         return this.each(function() {
-			// create and cache the gmapDirections object in the data cache for the map element
-			return $.data(this, 'gmapDirections') || $.data(this, 'gmapDirections', new gmapDirections(this, opt));
+            // create and cache the gmapDirections object in the data cache for the map element
+            return $.data(this, 'gmapDirections') || $.data(this, 'gmapDirections', new gmapDirections(this, opt));
         });
     };
 })(jQuery, window, document);
